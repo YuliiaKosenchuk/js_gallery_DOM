69c251a16586fd0367bd7a81099b32a94f4e920b
Yuliia Kosenchuk <yuliia.kosenchuk@gmail.com>
2024-08-05 15:39:30 +0200

    make build

---
+++ index.26c9a0b3.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#thumbs"),t=document.querySelector("#largeImg");e.addEventListener("click",function(e){var r=e.target.closest("a");r&&(t.src=r.href,t.alt=r.title,e.preventDefault())});
+//# sourceMappingURL=index.26c9a0b3.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,12 @@
+"use strict";
+const thumbs = document.querySelector("#thumbs");
+const imgDiv = document.querySelector("#largeImg");
+thumbs.addEventListener("click", function(e) {
+    const necessaryLink = e.target.closest("a");
+    if (!necessaryLink) return;
+    imgDiv.src = necessaryLink.href;
+    imgDiv.alt = necessaryLink.title;
+    e.preventDefault();
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
